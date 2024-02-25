d75c3e5a37ecf7948da849c02c70e448c340898e
Dmytro Torop <young-z@i.ua>
2024-02-25 14:38:43 +0200

    make build

---
+++ index.87f86529.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall");e.style.display="flex",e.style.justifyContent="center",e.style.alignItems="center",document.querySelector(".spider").style.position="relative";
+//# sourceMappingURL=index.87f86529.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wallElement = document.querySelector(".wall");
+wallElement.style.display = "flex";
+wallElement.style.justifyContent = "center";
+wallElement.style.alignItems = "center";
+const spiderElement = document.querySelector(".spider");
+spiderElement.style.position = "relative";
+
+//# sourceMappingURL=index.f75de5e1.js.map
